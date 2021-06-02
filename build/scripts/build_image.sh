4b857fd8972a233ffbec6ae17c7cb67fc461f228
noe132 <nnnnoe132@gmail.com>
2021-06-02 19:46:00 +0800

    fix: pipeline

--- build/scripts/build_image.sh
+++ build/scripts/build_image.sh
@@ -6,8 +6,6 @@ cd ../..
 IMAGE_NAME="rumsystem-fe:$BITBUCKET_BRANCH"
 REMOTE_IMAGE_NAME="dockerhub.qingcloud.com/pressone_private/rumsystem-fe:$BITBUCKET_BRANCH"
 
-./build/scripts/build.sh
-
 docker login -u prs-admin -p 57e348ab37aa5b55f68b7642ac584a41 dockerhub.qingcloud.com
 echo "building image..."
 docker build -f Dockerfile -t "$IMAGE_NAME" .
