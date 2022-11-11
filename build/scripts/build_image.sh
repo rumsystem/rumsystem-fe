85b20b079aa22fff22570ece3502805491a43f64
noe132 <nnnnoe132@gmail.com>
2022-11-11 14:33:04 +0800

    fix: build script

--- build/scripts/build_image.sh
+++ build/scripts/build_image.sh
@@ -3,8 +3,8 @@ set -e
 cd "$(dirname "$0")"
 cd ../..
 
-IMAGE_NAME="rumsystem-fe:$BITBUCKET_BRANCH"
-REMOTE_IMAGE_NAME="dockerhub.qingcloud.com/pressone_private/rumsystem-fe:$BITBUCKET_BRANCH"
+IMAGE_NAME="rumsystem-fe:release"
+REMOTE_IMAGE_NAME="dockerhub.qingcloud.com/pressone_private/rumsystem-fe:release"
 
 docker login -u prs-admin -p 57e348ab37aa5b55f68b7642ac584a41 dockerhub.qingcloud.com
 echo "building image..."
