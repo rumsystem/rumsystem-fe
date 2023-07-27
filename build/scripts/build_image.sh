330a5a83e990cbbd857a4971e813aff40ea9b0c8
okdaodine <btc.link.grt@gmail.com>
2023-07-27 14:59:54 +0800

    tweaks

--- build/scripts/build_image.sh
+++ build/scripts/build_image.sh
@@ -3,10 +3,9 @@ set -e
 cd "$(dirname "$0")"
 cd ../..
 
-IMAGE_NAME="rumsystem-fe:release"
-REMOTE_IMAGE_NAME="dockerhub.qingcloud.com/pressone_private/rumsystem-fe:release"
+IMAGE_NAME="rumsystem-fe"
+REMOTE_IMAGE_NAME="btclinkgrt/rumsystem-fe"
 
-docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PWD" dockerhub.qingcloud.com
 echo "building image..."
 docker build -f Dockerfile -t "$IMAGE_NAME" .
 echo "pushing image..."
